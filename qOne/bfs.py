91accb055e12b90c7b96aecbe35a455e59a95d85
Amulya <amulya.mummaneni@gmail.com>
2020-02-04 18:01:58 -0500

    bfs

--- qOne/bfs.py
+++ qOne/bfs.py
@@ -1,1 +1,9 @@
-class Bfs(AlgoSuper):
+import collections
+
+class Bfs(object):
+    def __init__(self): # update this with all fields
+        super(Bfs, self).__init__()
+
+    def bfs(self, maze):
+
+
