10c99953b2b2f20c8381dc1841b4d640ff0168f8
Conor <conortunc@gmail.com>
2020-02-05 19:13:36 -0500

    fixed fringe.

--- qOne/dfs.py
+++ qOne/dfs.py
@@ -14,15 +14,21 @@ class Dfs(object):
 		self.fringe.append((0,0))
 		self.prev[0][0] = (0,0)
 		maxFringe = 1
-		#use popleft() to get item from queue
+		#use pop() to get item from queue
 		while(self.fringe): # This checks if the dequeue is empty? python is such a mystical language...
-			item = self.fringe.popleft() # remove
+			item = self.fringe.pop() # remove
 			for i in self.validNeighbors(item): # get all valid neighbors,
 				if(self.maze.getGrid()[i[0]][i[1]] == 'g'):#if the neighbor is the goal
 					grid = self.maze.getGrid()
+					for ite in range(0,self.maze.getDim()):
+						for itj in range(0,self.maze.getDim()):
+							if(self.prev[ite][itj] is not None):
+								grid[ite][itj] = 'f'
+
 					backtrack = i
 					next = item
 					while(next!=backtrack):
+
 						grid[backtrack[0]][backtrack[1]] = '*'
 						backtrack = next
 						next = self.prev[backtrack[0]][backtrack[1]]
@@ -50,24 +56,30 @@ class Dfs(object):
 		i = item[0]
 		j = item[1]
 		grid = self.maze.getGrid()
+		# item above. If it is not outside of the maze, is a free spot, and has not been visited yet, we pass this check.
+		if i - 1 != -1 and (grid[i - 1][j] == 0 or grid[i - 1][j] == 'g') and self.prev[i - 1][j] is None:
+			ret.append((i - 1, j))
+
+		# left
+		if j - 1 != -1 and (grid[i][j - 1] == 0 or grid[i][j - 1] == 'g') and self.prev[i][j - 1] is None:
+			ret.append((i, j - 1))
+
+		# down
+		if i + 1 < len(grid) and (grid[i + 1][j] == 0 or grid[i + 1][j] == 'g') and self.prev[i + 1][j] is None:
+			ret.append((i + 1, j))
+
 		# to the right. If it is not outside of the maze, is a free spot, and has not been visited yet, we pass this check.
 		if j + 1 < len(grid) and (grid[i][j + 1] == 0 or grid[i][j + 1] == 'g') and self.prev[i][j + 1] is None:
 			ret.append((i, j+1))
 
-		#down
-		if i + 1 <len(grid) and (grid[i + 1][j] == 0 or grid[i + 1][j] == 'g') and self.prev[i + 1][j] is None:
-			ret.append((i + 1, j))
-		#left
-		if j - 1 != -1 and (grid[i][j-1] == 0 or grid[i][j-1] == 'g') and self.prev[i][j-1] is None:
-			ret.append((i, j-1))
 
-		#item above. If it is not outside of the maze, is a free spot, and has not been visited yet, we pass this check.
-		if i - 1 != -1 and (grid[i - 1][j] == 0 or grid[i - 1][j] == 'g')and self.prev[i - 1][j] is None:
-			ret.append((i-1, j))
+
+
+
 		return ret
 
 if __name__ == '__main__':
 
-	m = maze.Maze(100,.3)
+	m = maze.Maze(100,.1)
 	d = Dfs(m)
 
