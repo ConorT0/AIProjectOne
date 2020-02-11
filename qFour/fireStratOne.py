d73247e09d31a2e874325c763fe16e75a33922df
Alexander Goodkind <1017680+agoodkind@users.noreply.github.com>
2020-02-11 16:44:43 -0500

    comments for fireastar

--- qFour/fireStratOne.py
+++ qFour/fireStratOne.py
@@ -10,6 +10,10 @@ class fireAStar(fireMaze.FireMaze):
 		a = AStarManhatten.AStarManhatten(self.maze)
 		self.path = a.search()
 
+	# walk the solved path, and on each step update the cells that are on fire
+	# returns a list with how far we were able to get in the path
+	# if the entire list is returned that means we were successful
+	# TODO: paint how far we got in the maze & the fire
 	def walk_fire_maze(self):
 		for step in range(0, self.path):
 			r = self.path[step][0]
