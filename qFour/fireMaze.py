d2e1bfe8ad2db606aae6a4eac2ad959a3c75175a
Alexander Goodkind <1017680+agoodkind@users.noreply.github.com>
2020-02-11 16:41:42 -0500

    strat1firemaze done but untested

--- qFour/fireMaze.py
+++ qFour/fireMaze.py
@@ -1,16 +1,17 @@
-from qOne import maze
-
+import maze
 import random
 
 class FireMaze(maze.Maze):
-	def __init__(self, dim: int, probability: float, fireProbability: float):
-		super(FireMaze, self).__init__(dim, probability)
+	def __init__(self, dim: int, maze_probability: float, fireProbability: float):
+		super(FireMaze, self).__init__(dim, maze_probability)
 		self.fireProbability = fireProbability
 
 		# we don't want the first cell, and we don't want the last cell (dim - 1 is the last cell)
 		# randomly pick a cell to initially set on fire
 		self.updateCell('🔥', random.randint(1, self.dim - 2), random.randint(1, self.dim - 2))
 		#self.updateCell('🔥', 1,1)
+
+	# given a cell, find all the neighbors that are on fire
 	def count_on_fire_neighbors(self, cell: tuple) -> int:
 		r = cell[0]
 		c = cell[1]
@@ -33,17 +34,22 @@ class FireMaze(maze.Maze):
 
 		return k
 
-	# TODO: fill out what this method does
+	# updates the cells that are on fire with the given initial probability
 	def updateFire(self) -> None:
+
+		# create a list that will contain all the cells that are now caught on fire
 		cells_caught_on_fire = list()
-		for r in range(0,len(self.getGrid())):
-			for c in range(0,r):
+		for r in range(0, len(self.getGrid())):
+			for c in range(0, r):
 				k = self.count_on_fire_neighbors((r, c))
-				cell_probability = 1 - pow((1 - self.fireProbability),k)
-				if (random.uniform(0, 1) <= cell_probability):
+				cell_probability = 1 - pow((1 - self.fireProbability), k)
+				if random.uniform(0, 1) <= cell_probability:
 					cells_caught_on_fire.append((r, c))
+
+		# update the grid with the new cells
+		# we have to do this separately
 		for cell in cells_caught_on_fire:
-			self.updateCell('🔥', cell[0],cell[1])
+			self.updateCell('🔥', cell[0], cell[1])
 
 if __name__ == '__main__':
 	test = FireMaze(10, 0.1, 1)
--- qFour/fireStratOne.py
+++ qFour/fireStratOne.py
@@ -1,3 +1,23 @@
-from qOne import maze
+import maze
+import fireMaze
+import AStarManhatten
 
-class fireAStar(object):
+
+class fireAStar(fireMaze.FireMaze):
+
+	def __init__(self, dim: int, maze_probability: float, fireProbability: float):
+		super(fireAStar, self).__init__(dim, maze_probability, fireProbability)
+		a = AStarManhatten.AStarManhatten(self.maze)
+		self.path = a.search()
+
+	def walk_fire_maze(self):
+		for step in range(0, self.path):
+			r = self.path[step][0]
+			c = self.path[step][1]
+
+			if self.grid[r][c] == '🔥':
+				return self.path[0:step]
+
+			self.updateFire()
+
+		return self.path
